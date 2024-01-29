f86dbb1c2d7abdc4905221420e6967b0a4c1b9a5
uiok588 <88995290+uiok588@users.noreply.github.com>
2024-01-29 11:28:38 +0800

    Update head.js

--- pages/shared/head.js
+++ pages/shared/head.js
@@ -1,12 +1,14 @@
 head = `  
   <head>
+    <!--base為路徑基準設定-->
+    <base href="/EGNSS_DEMO/">
     <meta charset="UTF-8" />
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="~/lib/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="~/lib/fontawesome-free-6.2.1-web/css/all.min.css" />
-    <link rel="stylesheet" href="~/build/css/FrontStage.css" />
+    <link rel="stylesheet" href="lib/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="lib/fontawesome-free-6.2.1-web/css/all.min.css" />
+    <link rel="stylesheet" href="build/css/FrontStage.css" />
   </head>
 `;
 
