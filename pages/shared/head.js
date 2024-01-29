062c6bd86c93fe0214fcde74e2606ef526fe531b
uiok588 <88995290+uiok588@users.noreply.github.com>
2024-01-29 11:09:22 +0800

    Update head.js

--- pages/shared/head.js
+++ pages/shared/head.js
@@ -4,9 +4,9 @@ head = `
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="./lib/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="./lib/fontawesome-free-6.2.1-web/css/all.min.css" />
-    <link rel="stylesheet" href="./build/css/FrontStage.css" />
+    <link rel="stylesheet" href="lib/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="lib/fontawesome-free-6.2.1-web/css/all.min.css" />
+    <link rel="stylesheet" href="build/css/FrontStage.css" />
   </head>
 `;
 
